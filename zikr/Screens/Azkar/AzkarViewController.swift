db59b5b75fcc059c670c7b4fe6dafd0562674e81
aebai <aebai@allstate.com>
2018-05-21 13:32:09 -0500

    Update AzkarViewController to detect slider dragging

--- zikr/Screens/Azkar/AzkarViewController.swift
+++ zikr/Screens/Azkar/AzkarViewController.swift
@@ -12,10 +12,9 @@ class AzkarViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     @IBOutlet weak var pageSelector: UISegmentedControl!
     @IBOutlet weak var playPause: UIButton!
-    
     @IBOutlet weak var audioSlider: UISlider!
-    var audioPlayer: AVAudioPlayer?
     
+    var audioPlayer: AVAudioPlayer?
     let viewModel = ZikrQuranViewModel()
     
     override func viewDidLoad() {
@@ -23,35 +22,28 @@ class AzkarViewController: UIViewController {
         configureTableView()
         configureAudioPlayer()
     }
-
+    
     private func configureTableView() {
         let zikrCellNib = UINib(nibName: "ZikrTableViewCell", bundle: nil)
         tableView.register(zikrCellNib, forCellReuseIdentifier: "zikrCell")
     }
     
     private func configureAudioPlayer() {
-        
         let sound = Bundle.main.url(forResource: "112", withExtension: "mp3")
-
+        
         guard let player = try? AVAudioPlayer(contentsOf: sound!) else {
             return
         }
         audioPlayer = player
-        print("audioPlayer duration: \(audioPlayer?.duration)")
         audioPlayer?.delegate = self
-
-        if let duration = audioPlayer?.duration.magnitude {
-            audioSlider.maximumValue = Float(duration)
-        }
-        
-        audioSlider.maximumValue = Float((audioPlayer?.duration)!)
+        audioSlider.maximumValue = Float(audioPlayer!.duration)
     }
     
     @IBAction func playPauseTapped(_ sender: UIButton) {
         guard let isPlaying = audioPlayer?.isPlaying else {
             return
         }
-    
+        
         if playPause.imageView?.image == #imageLiteral(resourceName: "play") {
             playPause.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
             playPause.imageView?.image = #imageLiteral(resourceName: "pause")
@@ -69,23 +61,28 @@ class AzkarViewController: UIViewController {
     @IBAction func pageSelectorTapped(_ sender: UISegmentedControl) {
         tableView.reloadData()
     }
+    
+    @IBAction func audioSliderValueChanged(_ sender: UISlider) {
+        audioPlayer?.currentTime =  Double(sender.value)
+    }
+    
 }
 
 extension AzkarViewController: UITableViewDataSource {
     func numberOfSections(in _: UITableView) -> Int {
         return 1
     }
-
+    
     func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
         return viewModel.morningZikrModels.count
     }
-
+    
     func tableView(_: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "zikrCell") as! ZikrTableViewCell
         cell.delegate = self
         
         let zikrModel = pageSelector.selectedSegmentIndex == 0 ? viewModel.morningZikrModels[indexPath.row] : viewModel.eveningZikrModels[indexPath.row]
-
+        
         cell.configureUI(zikrModel: zikrModel)
         return cell
     }
@@ -99,7 +96,7 @@ extension AzkarViewController: ZikrTableViewCellDelegate {
         activityController.setValue("Zikr application would like to share the following content with you", forKey: "Subject")
         present(activityController, animated: true)
     }
-
+    
     func zikrTableViewRedrawCell(cell: ZikrTableViewCell) {
         tableView.beginUpdates()
         tableView.endUpdates()
@@ -110,6 +107,7 @@ extension AzkarViewController: AVAudioPlayerDelegate {
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
         if flag {
             playPause.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+            audioSlider.value = 0.0
         }
     }
 }
